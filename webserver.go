b7dc7a4e724ece9dedf200dce64db3d1bc7b1dd6
Althy <agkljm@gmail.com>
2014-10-11 15:32:50 +0200

    websever zorking

--- webserver.go
+++ webserver.go
@@ -1,5 +1,21 @@
-package ATYmgur
+package main
 
 //Web Server in Progress : #BetterthanApache
 //m
-import "net/http"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, Welcome to %s!", r.URL.Path[1:])
+}
+
+func startWeb() {
+	fmt.Println("Starting WebServer")
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/settings", SettingsHandler)
+	r.HandleFunc("/callback", CallbackHandler)
+	http.ListenAndServe(":8181", nil)
+}
