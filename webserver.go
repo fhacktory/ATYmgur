43bb295f8a70b94df91f1e3d3c45ea6f3a671116
Althy <agkljm@gmail.com>
2014-10-11 17:56:13 +0200

    webserver : handlers OK

--- webserver.go
+++ webserver.go
@@ -1,21 +1,32 @@
 package main
 
 //Web Server in Progress : #BetterthanApache
-//m
+
 import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, Welcome to %s!", r.URL.Path[1:])
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, Welcome to Home!", r.URL.Path[1:])
+}
+
+func Settings(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, Set your %s!", r.URL.Path[1:])
+}
+
+func Callback(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, %s page!", r.URL.Path[1:])
 }
 
 func startWeb() {
+	r := mux.NewRouter()
 	fmt.Println("Starting WebServer")
-	r.HandleFunc("/", HomeHandler)
-	r.HandleFunc("/settings", SettingsHandler)
-	r.HandleFunc("/callback", CallbackHandler)
+	r.HandleFunc("/", Home)
+	r.HandleFunc("/settings", Settings)
+	r.HandleFunc("/callback", Callback)
+	http.Handle("/", r)
 	http.ListenAndServe(":8181", nil)
+	fmt.Println("Stoping WebServer")
 }
